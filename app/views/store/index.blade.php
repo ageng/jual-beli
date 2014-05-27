dabe7e2db56bbcc1500fa022a7d71a0088bf44ed
Ageng <newbie.masih01@gmail.com>
2014-05-28 02:23:50 +0700

    add sementara

---
+++ app/views/store/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+
+@section('promo')
+
+	<section id="promo">
+		<div id="promo-details">
+			<h1>Today;s Deals</h1>
+			<p>Checkout this section of<br/>
+				products at discounted price.</p>
+			<a href="#" class="default-btn">Shop Now</a>
+		</div>
+		{{ HTML::image('img/promo.png', 'Promotional Ad') }}
+	</section>
+
+@stop
+
+@section('content')
+
+	
+
+@stop
