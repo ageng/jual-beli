b987ec1dc079f671dc2b665cb3e22cbc89d0af24
Ageng <newbie.masih01@gmail.com>
2014-05-28 09:54:21 +0700

    lanjutan

--- app/controllers/StoreController.php
+++ app/controllers/StoreController.php
@@ -8,7 +8,7 @@ class StoreController extends BaseController {
 
 	public function getIndex() {
 		return View::make('store.index')
-			->with('products', Product::take->(4)->orderBy('created_at', 'DESC')->get());
+			->with('products', Product::take(4)->orderBy('created_at', 'DESC')->get());
 	}
 
 	public function getView($id) {
--- app/views/store/index.blade.php
+++ app/views/store/index.blade.php
@@ -16,6 +16,35 @@
 
 @section('content')
 
-	
+	<h2>New Products</h2>
+    <hr>
+    <div id="products">
+    	@foreach($products as $product)
+        <div class="product">
+            <a href="/store/view/{{ $product->id }}">
+            	{{ HTML::image($product->image, $product->title, array('class'=>'feature', 'width'=>'240', 'height'=>'127')) }}
+            </a>
+
+            <h3><a href="/store/view/{{ $product->id }}">{{ $product->title }}</a></h3>
+
+            <p>{{ $product->description }}</p>
+
+            <h5>
+            	Availability: 
+            	<span class="{{ Availability::displayClass($product->availability) }}">
+            		{{ Availability::display($product->availability) }}
+            	</span>
+            </h5>
+
+            <p>
+               <a href="#" class="cart-btn">
+                  <span class="price">{{ $product->price }}</span> 
+                   {{ HTML::image('img/white-cart.gif', 'Add to Cart') }} 
+                   ADD TO CART
+               </a>
+            </p>
+        </div>
+        @endforeach
+    </div><!-- end products -->
 
 @stop
